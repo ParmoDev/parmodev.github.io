9f984e040f0cd8be024112843dcad3cb01682b4b
Magnus Parmo <73383707+ParmoDev@users.noreply.github.com>
2021-03-09 23:00:50 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,25 @@
+let loggedIn = false
+
+function loginHref() {
+    location.href = "login.html"
+}
+
 function loginCheck() {
-    console.log("I aint empty boa")
+    if (document.getElementById("username").value == "DashboardAdminAccess" && document.getElementById("password").value == "GAqTd?qrtX5j873Q") {
+        loggedIn = true
+        window.location.assign("success.html")
+        console.log("Logged in!")
+
+    } else {
+        console.log("FAILED")
+        window.location.assign("index.html")
+    }
+}
+
+function loggedInCheck() {
+    if (document.URL.includes("success.html") && loggedIn == true) {
+        location.href = "dashboard.html"
+    } else {
+        location.href = "login.html"
+    }
 }
